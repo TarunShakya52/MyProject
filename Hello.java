383bb31e0049e07ff915172957001a65f876f8a3
Tarun Shakya <tarun.tshakya@gmail.com>
2024-06-24 17:04:24 +0530

    commit 1.0

---
+++ Hello.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Hello{
+    public static void main(String a[]){
+        int num1 = 10;
+       
+        int num2 = 5;
+
+        
+
+       while (num2 < num1) {
+
+        System.err.println("Jagdish");
+        num1--;
+
+       }
+    }
+
+}
